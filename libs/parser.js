04148b5d7dca9d9486be086cb274ea84ce7b2e9f
boronin <ryzii@yandex.ru>
2020-03-28 10:55:28 +0300

    increase curl timeout

--- libs/parser.js
+++ libs/parser.js
@@ -16,7 +16,7 @@ function getNumber(text) {
 async function getCity(url, cityCode) {
     let body;
     try {
-        const { stdout } = await exec(`curl -m 20 -A "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1" https://${url}?cityId=City${cityCode}`, {maxBuffer: 1024 * 5000});
+        const { stdout } = await exec(`curl -m 60 -A "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1" https://${url}?cityId=City${cityCode}`, {maxBuffer: 1024 * 5000});
         body = stdout;
     } catch (err) {
         const errorMessage = `Ошибка загрузки товара ${url} в городе ${cityCode}`;
