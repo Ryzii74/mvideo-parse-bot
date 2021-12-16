275774d1dbb0496ac1f003177084f0a326dbf237
boronin <ryzii@yandex.ru>
2021-12-16 12:14:38 +0500

    update

--- libs/goods.js
+++ libs/goods.js
@@ -1,93 +1,60 @@
 "use strict";
 
-const db = require('./db');
-const config = require('../config');
-
-const PAGE_SIZE = 25;
-
-function getCollection() {
-    return db.get().collection(config.collections.goods);
-}
+const goodsListStorage = require("../storages/goodListStorage");
+const goodsDataStorage = require('../storages/goodDataStorage');
+const parser = require("./parser");
+const notifier = require('../core/notifier');
 
 module.exports = {
-    async compareAndUpdate(good) {
-        let goodDocument = await this.getByLink(good.link);
-        if (!goodDocument) goodDocument = {};
+  async checkGoods(cityCode) {
+    const links = await goodsListStorage.getAllLinks();
+    console.log('links to check', links);
 
-        if (good.existed !== goodDocument.existed) {
-            return { exist: { isExisted: good.existed } };
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      try {
+        const goodData = await parser.get(link, cityCode);
+        const currentGoodData = await goodsDataStorage.getByLink(link);
+
+        const diff = getGoodDiff(goodData, currentGoodData);
+        if (diff) {
+          await goodsDataStorage.save(goodData);
+          await notifier.sendAll(diff);
         }
+      } catch (err) {
+        console.error(err);
+        await notifier.sendAll(err.message);
+      }
 
-        if (good.chelPrice === goodDocument.chelPrice
-            && good.chelAvailable === goodDocument.chelAvailable
-            && good.chelIsTradeIn === goodDocument.chelIsTradeIn
-            && good.chelBonus === goodDocument.chelBonus) {
-            return {};
-        }
-
-        await getCollection().updateOne({ link: good.link }, { $set: good }, { upsert: true });
-        return {
-            compare: {
-                chelPriceBefore: goodDocument.chelPrice || 'Неизвестно',
-                chelPriceAfter: good.chelPrice,
-                chelAvailableBefore: goodDocument.chelAvailable,
-                chelAvailableAfter: good.chelAvailable,
-                chelIsTradeInBefore: goodDocument.chelIsTradeIn,
-                chelIsTradeInAfter: good.chelIsTradeIn,
-                chelBonusBefore: goodDocument.chelBonus,
-                chelBonusAfter: good.chelBonus,
-            },
-        };
-    },
-
-    async getCount() {
-        return getCollection().count({});
-    },
-
-    async getLastNumber() {
-        const lastCreatedDocument = await getCollection()
-          .find({}, { number: 1 })
-          .limit(1)
-          .sort({ _id: -1 })
-          .toArray();
-        return (lastCreatedDocument[0] && lastCreatedDocument[0].number) || 0;
-    },
-
-    async getByLink(link) {
-        return getCollection().findOne({ link });
-    },
-
-    async add(good) {
-        const goodDocument = await this.getByLink(good.link);
-        if (goodDocument) return;
-
-        let lastNumber = await this.getLastNumber();
-        await getCollection().insertOne({
-            ...good,
-            createdAt: new Date(),
-            number: lastNumber + 1,
-        });
-    },
-
-    getData(data) {
-        return `Цена: ${data.chelPrice}\nДоступность: ${data.chelAvailable ? 'Да' : 'Нет'}\nTrade-in: ${data.chelIsTradeIn ? 'Да' : 'Нет'}\nБонусные рубли: ${data.chelBonus}`;
-    },
-
-    async getAllLinks() {
-        return getCollection().find({}, { link: 1, number: 1 }).toArray();
-    },
-
-    async getLinksPage(pageNumber) {
-        if (pageNumber < 1) pageNumber = 1;
-
-        return getCollection()
-            .find({}, { link: 1, number: 1 })
-            .skip((pageNumber - 1) * PAGE_SIZE)
-            .limit(PAGE_SIZE)
-            .toArray();
-    },
-
-    async remove(number) {
-        await getCollection().removeOne({ number });
+      await wait(Math.floor((Math.random() + 1) * 30 * 1000));
     }
+  }
 };
+
+function getGoodDiff(updated, current) {
+  if (updated.price === current.price
+    && updated.isAvailable === current.isAvailable
+    && updated.bonus === current.bonus
+    && updated.isExisted === current.isExisted) {
+    return null;
+  }
+
+  if (updated.isExisted === false) {
+    return `Товар ${updated.link} пропал из магазина`;
+  }
+
+  let message = `Изменения в товаре: ${updated.link}`;
+  message += `\nЦена - ${current.price || '?'} -> ${updated.price}`;
+  message += `\nДоступность - ${flagToMessage(current.isAvailable)} -> ${flagToMessage(updated.isAvailable)}`;
+  message += `\nБонусные рубли - ${current.bonus || '?'} -> ${updated.bonus}`;
+  return message;
+}
+
+function wait(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+function flagToMessage(value) {
+  if (value === undefined) return '?';
+  return value ? 'да' : 'нет';
+}
